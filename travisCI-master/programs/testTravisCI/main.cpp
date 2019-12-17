69af9d8716060a7723441d6954d0e9b2100e1f76
heamaris <heamaris@poligran.edu.co>
2019-12-16 21:12:24 -0500

    travis

---
+++ travisCI-master/programs/testTravisCI/main.cpp
@@ -0,0 +1,22 @@
+/*
+*	Prueba de uso Travis CI
+*
+*	@davidvelascogarcia
+*
+*
+*/
+
+//  Librerias
+
+#include <iostream>
+
+//  Espacio de nombres
+
+
+using namespace std;
+
+int main(){
+
+ cout<<"Prueba de uso Travis CI"<<endl;
+
+}
